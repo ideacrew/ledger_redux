bfd0e109375e572cc205f26706a4441f88764f65
harshaellanki <h_vardhan@yahoo.com>
2020-08-17 13:33:04 -0400

    fixed rackattack initializer

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,6 +1,6 @@
 # Subscribe to RackAttack notification throttle events and send to Rails Logger
 ActiveSupport::Notifications.subscribe("rack.attack") do |name, start, finish, request_id, req|
-  if req.env['rack.attack.match_type'] == :throttle
+  if req[:request].env['rack.attack.match_type'] == :throttle
     Rails.logger.info "Throttled IP: #{req.ip}"
   end
 end
